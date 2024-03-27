efe177f0b5bd45377a016bdd5c26347bd3fa0aaa
pro-masud <promasudbd@gmail.com>
2024-03-27 23:31:48 +0600

    testimonial slider creation complete

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        loop:true,
+        margin:50,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },            
+            960:{
+                items:5
+            },
+            1200:{
+                items:4
+            }
+        }
+    });
+    owl.on('mousewheel', '.owl-stage', function (e) {
+        if (e.deltaY>0) {
+            owl.trigger('next.owl');
+        } else {
+            owl.trigger('prev.owl');
+        }
+        e.preventDefault();
+    });
+  });
